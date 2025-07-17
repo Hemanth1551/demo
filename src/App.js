75ce55dcc8b5a5bbf808b0eefff4d8969d677672
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-07-17 15:01:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,47 @@ import { useEffect } from 'react';
 
 function App() {
 
+  // const [location, setLocation] = useState(null);
+  // const [inCollege, setInCollege] = useState(null);
+
+  const COLLEGE_COORDS = {
+    lat: 16.5561096,
+    lng: 81.9749443
+  };
+  const MAX_DISTANCE_KM = 0.2;
+
+
+  function getDistanceFromCollege(lat1, lon1) {
+    const R = 6371;
+    const lat2 = COLLEGE_COORDS.lat;
+    const lon2 = COLLEGE_COORDS.lng;
+
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+
+    const a = Math.sin(dLat / 2) ** 2 +
+              Math.cos(lat1 * Math.PI / 180) *
+              Math.cos(lat2 * Math.PI / 180) *
+              Math.sin(dLon / 2) ** 2;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  }
+
   useEffect(() => {
       navigator.geolocation.getCurrentPosition(
     (position) => {
       console.log(position.coords.latitude, position.coords.longitude);
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      const h = getDistanceFromCollege(lat, long);
+      if (h <= MAX_DISTANCE_KM) {
+            alert("your in clg");
+          } else {
+            alert("not clg");
+          }
       alert(position.coords.latitude +" "+ position.coords.longitude);
+
     },
     (err) => {
       console.error(err);
